831530911617c66a4a1922427c82dd019ff3a0f1
IamPete1 <33176108+IamPete1@users.noreply.github.com>
2018-11-18 16:14:54 +0000

    AC_AttitudeControl: add acceleration offset for underslung load contol

--- libraries/AC_AttitudeControl/AC_PosControl.h
+++ libraries/AC_AttitudeControl/AC_PosControl.h
@@ -11,6 +11,7 @@
 #include "AC_AttitudeControl.h" // Attitude control library
 #include <AP_Motors/AP_Motors.h>          // motors library
 #include <AP_Vehicle/AP_Vehicle.h>         // common vehicle parameters
+#include <AP_UnderSlung/AP_UnderSlung.h>
 
 
 // position controller default definitions
