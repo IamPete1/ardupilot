0578d1c727e1531f107a36dd5225a80ef5c558c0
Peter Barker <pbarker@barker.dropbear.id.au>
2019-11-28 09:34:21 +1100

    autotest: test buttons on Plane and Copter as well as Rover

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -1464,6 +1464,10 @@ class AutoTestPlane(AutoTest):
              "Test ADSB",
              self.test_adsb),
 
+            ("Button",
+             "Test Buttons",
+             self.test_button),
+
             ("AdvancedFailsafe",
              "Test Advanced Failsafe",
              self.test_advanced_failsafe),
