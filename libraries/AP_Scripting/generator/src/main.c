720a237d21cb90cb40abfc7494f6d1ff3ae220a1
Peter Hall <iampete@hotmail.co.uk>
2021-02-13 02:57:44 +0000

    AP_Scripting: generator: support nullable arguments that should be pased as a pointer

--- libraries/AP_Scripting/generator/src/main.c
+++ libraries/AP_Scripting/generator/src/main.c
@@ -28,6 +28,7 @@ char keyword_literal[]             = "literal";
 char keyword_attr_enum[]    = "'enum";
 char keyword_attr_literal[] = "'literal";
 char keyword_attr_null[]    = "'Null";
+char keyword_attr_null_ptr[] = "'Null_ptr";
 char keyword_attr_reference[]  = "'Ref";
 
 // type keywords
@@ -143,6 +144,7 @@ enum type_flags {
   TYPE_FLAGS_NULLABLE = (1U << 1),
   TYPE_FLAGS_ENUM     = (1U << 2),
   TYPE_FLAGS_REFERNCE = (1U << 3),
+  TYPE_FLAGS_NULLABLE_POINTER = (1U << 4),
 };
 
 struct type {
@@ -498,11 +500,15 @@ int parse_type(struct type *type, const uint32_t restrictions, enum range_check_
       type->flags |= TYPE_FLAGS_ENUM;
     } else if (strcmp(attribute, keyword_attr_literal) == 0) {
       type->type = TYPE_LITERAL;
-    } else if (strcmp(attribute, keyword_attr_null) == 0) {
+    } else if ((strcmp(attribute, keyword_attr_null) == 0) || (strcmp(attribute, keyword_attr_null_ptr) == 0)) {
       if (restrictions & TYPE_RESTRICTION_NOT_NULLABLE) {
         error(ERROR_USERDATA, "%s is not nullable in this context", data_type);
       }
-      type->flags |= TYPE_FLAGS_NULLABLE;
+      if (strcmp(attribute, keyword_attr_null_ptr) == 0) {
+        type->flags |= TYPE_FLAGS_NULLABLE_POINTER;
+      } else {
+        type->flags |= TYPE_FLAGS_NULLABLE;
+      }
     } else if (strcmp(attribute, keyword_attr_reference) == 0) {
       type->flags |= TYPE_FLAGS_REFERNCE;
     } else {
@@ -553,7 +559,7 @@ int parse_type(struct type *type, const uint32_t restrictions, enum range_check_
   }
 
   // sanity check that only supported types are nullable
-  if (type->flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE)) {
+  if (type->flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE | TYPE_FLAGS_NULLABLE_POINTER)) {
     // a switch is a very verbose way to do this, but forces users to consider new types added
     switch (type->type) {
       case TYPE_FLOAT:
@@ -571,7 +577,7 @@ int parse_type(struct type *type, const uint32_t restrictions, enum range_check_
       case TYPE_AP_OBJECT:
       case TYPE_LITERAL:
       case TYPE_NONE:
-        if (type->flags & TYPE_FLAGS_NULLABLE) {
+        if (type->flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_NULLABLE_POINTER)) {
           error(ERROR_USERDATA, "%s types cannot be nullable", data_type);
         } else {
           error(ERROR_USERDATA, "%s types cannot be passed as reference", data_type);
@@ -581,7 +587,7 @@ int parse_type(struct type *type, const uint32_t restrictions, enum range_check_
   }
 
   // add range checks, unless disabled or a nullable type
-  if (range_type != RANGE_CHECK_NONE && !(type->flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE))) {
+  if (range_type != RANGE_CHECK_NONE && !(type->flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE | TYPE_FLAGS_NULLABLE_POINTER))) {
     switch (type->type) {
       case TYPE_FLOAT:
       case TYPE_INT8_T:
@@ -679,14 +685,14 @@ void handle_method(char *parent_name, struct method **methods) {
     if (arg_type.type == TYPE_NONE) {
       error(ERROR_USERDATA, "Can't pass an empty argument to a method");
     }
-    if ((method->return_type.type != TYPE_BOOLEAN) && (arg_type.flags & TYPE_FLAGS_NULLABLE)) {
+    if ((method->return_type.type != TYPE_BOOLEAN) && (arg_type.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_NULLABLE_POINTER))) {
       error(ERROR_USERDATA, "Nullable arguments are only available on a boolean method");
     }
     if ((method->return_type.type == TYPE_BOOLEAN) && (arg_type.flags & TYPE_FLAGS_REFERNCE)) {
       error(ERROR_USERDATA, "Use Nullable arguments on a boolean method, not 'Ref");
     }
-    if (arg_type.flags & TYPE_FLAGS_NULLABLE) {
-      method->flags |= TYPE_FLAGS_NULLABLE;
+    if (arg_type.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_NULLABLE_POINTER)) {
+      method->flags |= (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_NULLABLE_POINTER);
     }
     if (arg_type.flags & TYPE_FLAGS_REFERNCE) {
       method->flags |= TYPE_FLAGS_REFERNCE;
@@ -1053,7 +1059,7 @@ void emit_checker(const struct type t, int arg_number, int skipped, const char *
     error(ERROR_INTERNAL, "Can't handle more then %d arguments to a function", NULLABLE_ARG_COUNT_BASE);
   }
 
-  if (t.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE)) {
+  if (t.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE | TYPE_FLAGS_NULLABLE_POINTER)) {
     arg_number = arg_number + NULLABLE_ARG_COUNT_BASE;
     switch (t.type) {
       case TYPE_BOOLEAN:
@@ -1346,7 +1352,7 @@ int emit_references(const struct argument *arg, const char * tab) {
   int arg_index = NULLABLE_ARG_COUNT_BASE + 2;
   int return_count = 0;
   while (arg != NULL) {
-    if (arg->type.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE)) {
+    if (arg->type.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE | TYPE_FLAGS_NULLABLE_POINTER)) {
       return_count++;
       switch (arg->type.type) {
         case TYPE_BOOLEAN:
@@ -1414,7 +1420,7 @@ void emit_userdata_method(const struct userdata *data, const struct method *meth
   // sanity check number of args called with
   arg_count = 1;
   while (arg != NULL) {
-    if (!(arg->type.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE)) && !(arg->type.type == TYPE_LITERAL)) {
+    if (!(arg->type.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE | TYPE_FLAGS_NULLABLE_POINTER)) && !(arg->type.type == TYPE_LITERAL)) {
       arg_count++;
     }
     arg = arg->next;
@@ -1449,7 +1455,7 @@ void emit_userdata_method(const struct userdata *data, const struct method *meth
       arg_count++;
     }
     if (//arg->type.type == TYPE_LITERAL ||
-        arg->type.flags & (TYPE_FLAGS_NULLABLE| TYPE_FLAGS_REFERNCE)) {
+        arg->type.flags & (TYPE_FLAGS_NULLABLE| TYPE_FLAGS_REFERNCE | TYPE_FLAGS_NULLABLE_POINTER)) {
       skipped++;
     }
     arg = arg->next;
@@ -1561,7 +1567,7 @@ void emit_userdata_method(const struct userdata *data, const struct method *meth
       case TYPE_ENUM:
       case TYPE_USERDATA:
       case TYPE_AP_OBJECT:
-        fprintf(source, "            data_%d", arg_count + ((arg->type.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE)) ? NULLABLE_ARG_COUNT_BASE : 0));
+        fprintf(source, "            %sdata_%d", (arg->type.flags & TYPE_FLAGS_NULLABLE_POINTER) ? "&" : "", arg_count + ((arg->type.flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_REFERNCE | TYPE_FLAGS_NULLABLE_POINTER)) ? NULLABLE_ARG_COUNT_BASE : 0));
         break;
       case TYPE_LITERAL:
         fprintf(source, "            %s", arg->type.data.literal);
@@ -1602,7 +1608,7 @@ void emit_userdata_method(const struct userdata *data, const struct method *meth
 
   switch (method->return_type.type) {
     case TYPE_BOOLEAN:
-      if (method->flags & TYPE_FLAGS_NULLABLE) {
+      if (method->flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_NULLABLE_POINTER)) {
         fprintf(source, "    if (data) {\n");
         // we need to emit out nullable arguments, iterate the args again, creating and copying objects, while keeping a new count
         arg = method->arguments;
@@ -1653,7 +1659,7 @@ void emit_userdata_method(const struct userdata *data, const struct method *meth
       break;
   }
 
-  if ((method->return_type.type != TYPE_BOOLEAN) || ((method->flags & TYPE_FLAGS_NULLABLE) == 0)) {
+  if ((method->return_type.type != TYPE_BOOLEAN) || ((method->flags & (TYPE_FLAGS_NULLABLE | TYPE_FLAGS_NULLABLE_POINTER)) == 0)) {
       fprintf(source, "    return %d;\n", return_count);
   }
 
