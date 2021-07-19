ee8c86792e2c0ab51ba21c87368388b603b0cb25
Iampete1 <iampete@hotmail.co.uk>
2021-07-19 19:23:26 +0100

    GCS_MAVLink: pass commands to scripting, blocking passing on to AP

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -4918,6 +4918,14 @@ void GCS_MAVLINK::handle_command_long(const mavlink_message_t &msg)
     mavlink_command_long_t packet;
     mavlink_msg_command_long_decode(&msg, &packet);
 
+#ifdef AP_SCRIPTING_ENABLED
+    AP_Scripting *scripting = AP_Scripting::get_singleton();
+    if (scripting != nullptr && scripting->handle_command_long(packet, chan)) {
+        // Scripting is listening for this command don't process
+        return;
+    }
+#endif
+
     hal.util->persistent_data.last_mavlink_cmd = packet.command;
 
     MAV_RESULT result;
@@ -5140,6 +5148,14 @@ void GCS_MAVLINK::handle_command_int(const mavlink_message_t &msg)
     // decode packet
     mavlink_command_int_t packet;
     mavlink_msg_command_int_decode(&msg, &packet);
+
+#ifdef AP_SCRIPTING_ENABLED
+    AP_Scripting *scripting = AP_Scripting::get_singleton();
+    if (scripting != nullptr && scripting->handle_command_int(packet, chan)) {
+        // Scripting is listening command don't process
+        return;
+    }
+#endif
 
     hal.util->persistent_data.last_mavlink_cmd = packet.command;
 
