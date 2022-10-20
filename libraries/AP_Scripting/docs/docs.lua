0faa6c972d3b95bcf495cea7a1111b17f1ec055d
Peter Hall <iampete@hotmail.co.uk>
2022-10-20 23:16:28 +0100

    AP_Scripting: add bindings to get throttle, forward, lateral and spool state from motors

--- libraries/AP_Scripting/docs/docs.lua
+++ libraries/AP_Scripting/docs/docs.lua
@@ -736,6 +736,27 @@ function mount:get_mode(instance) end
 ---@class motors
 motors = {}
 
+-- get lateral motor output
+---@return number
+function motors:get_lateral() end
+
+-- get forward motor output
+---@return number
+function motors:get_forward() end
+
+-- get throttle motor output
+---@return number
+function motors:get_throttle() end
+
+-- get throttle motor output
+---@return integer
+---| '0' # Shut down
+---| '1' # Ground idle
+---| '2' # Spooling up
+---| '3' # Throttle unlimited
+---| '4' # Spooling down
+function motors:get_spool_state() end
+
 -- desc
 ---@param param1 string
 function motors:set_frame_string(param1) end
