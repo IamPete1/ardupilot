32f6afa8c83f6cd35197307533ef379ca1920243
bnsgeyer <bnsgeyer@aol.com>
2026-03-25 23:16:44 -0400

    AP_MotorsHeli: keep runup_complete and spooldown_complete in RSC

--- libraries/AP_Motors/AP_MotorsHeli.h
+++ libraries/AP_Motors/AP_MotorsHeli.h
@@ -174,12 +174,15 @@ protected:
     AP_MotorsHeli_RSC   _main_rotor;            // main rotor
 
     // update_motor_controls - sends commands to motor controllers
-    virtual void update_motor_control(AP_MotorsHeli_RSC::DesiredRSCSpoolState state) = 0;
+    virtual AP_Motors::SpoolState update_motor_control(AP_MotorsHeli_RSC::DesiredRSCSpoolState state) = 0;
 
     // Converts AP_Motors::SpoolState from _spool_state variable to AP_MotorsHeli_RSC::RotorControlState
     AP_MotorsHeli_RSC::DesiredRSCSpoolState get_rotor_control_state() const;
 
-    // run spool logic
+    // Converts AP_MotorsHeli_RSC::RSCSpoolState from _rotor_spool_state variable to AP_Motors::SpoolState
+    AP_Motors::SpoolState convert_spool_state(AP_MotorsHeli_RSC::RSCSpoolState rotor_spool_state) const;
+
+// run spool logic
     void                output_logic();
 
     // output_to_motors - sends commands to the motors
